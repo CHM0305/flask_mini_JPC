2cea12e1d53a758fd745f8f96964c9e01621bf9e
Choi_Hye_Min <chym0305@naver.com>
2025-01-15 02:17:03 +0900

    user 함수 수정 및 구현 완료

--- oz_form/app/views/users.py
+++ oz_form/app/views/users.py
@@ -9,9 +9,9 @@ user_blp = Blueprint('users', 'users', url_prefix='/users', description='Operati
 @user_blp.route('/')
 class UserList(MethodView):
     def get(self):
-        users = User.query.all()
-        user_data = [user.to_dict() for user in users]
-        return jsonify(user_data)
+        data = User.query.all()
+        users_data = [user.to_dict() for user in data]
+        return jsonify(users_data)
 
     def post(self):
         user_data = request.json
@@ -34,10 +34,32 @@ class UserList(MethodView):
 
 @user_blp.route('/<int:user_id>')
 class UserResource(MethodView):
+    #특정 아이디 조회
     def get(self, user_id):
         user = User.query.get_or_404(user_id)
         return jsonify(user.to_dict()),200
-    
+    #특정 아이디 수정
+    def put(self, user_id):
+        user=User.query.get(user_id)
+        if not user:
+            return jsonify({"error": f"User with ID {user_id} not found"}), 404
+
+        data=request.json
+        user.name=data.get["name"],
+        user.age=data.get["age"],
+        user.email=data.get["email"],
+        user.gender=data.get["gender"]
+        db.session.commit()
+        return jsonify({"message": "User updated successfully"}), 200
+
+    def delete(self, user_id):
+        user=User.query.get(user_id)
+        if not user:
+            return jsonify({"error": f"User with ID {user_id} not found"}), 404
+
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": "User deleted successfully"}), 204
 
 #아이디 생성, 조회 완료
-#특정 아이디 조회 완료
+#특정 아이디 조회 수정 삭제 완료
