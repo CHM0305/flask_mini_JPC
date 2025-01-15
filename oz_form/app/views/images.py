bc65a07008a3409677550f12e75c81216be82a29
Choi_Hye_Min <chym0305@naver.com>
2025-01-15 12:18:26 +0900

    이미지 파일 수정

--- oz_form/app/views/images.py
+++ oz_form/app/views/images.py
@@ -60,10 +60,6 @@ class ImageResource(MethodView):
 
         db.session.delete(image)
         db.session.commit()
-<<<<<<< HEAD
         return jsonify({"message": "Image deleted successfully"}), 204
-=======
-        return jsonify({"message": "Image deleted successfully"}), 204
->>>>>>> d9031b5f32baf3354385d65704abf2ae35567a00
 
 #이미지 수정 조회/ 특정 이미지 수정 조회 삭제 완료
