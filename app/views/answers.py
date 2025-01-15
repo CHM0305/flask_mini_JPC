aa59309d94c72add81c947d3a61add6fba3cfbc8
Choi_Hye_Min <chym0305@naver.com>
2025-01-15 21:53:33 +0900

    답변 수정

--- app/views/answers.py
+++ app/views/answers.py
@@ -4,9 +4,8 @@ from flask_smorest import Blueprint
 from config import db
 from app.models import Answer
 
-answer_blp = Blueprint('Answer', 'answer', url_prefix='/answer')
+answer_blp = Blueprint('Answer', 'answer', url_prefix='/submit')
 
-# 응답 저장
 @answer_blp.route('/')
 class AnswerList(MethodView):
     # 모든 답변 조회 - 사용자들이 어떤 답변들을 했는지 통계적으로 알기 위함 
@@ -23,4 +22,26 @@ class AnswerGet(MethodView):
         if not answers:
             return{"massage":"No found data"}
         return [answer.to_dict() for answer in answers]
-    
+
+@answer_blp.route('/',method=['POST'])
+class AnswerPost(MethodView):
+    def post(self):
+        #user=User.query.get("user_id") 이런식으로 갖고 오면 전체적으로 가지고 오는 것이라 한 아이디만을 추출하기 어려움.
+        #choices=Choices.query.get("choices_id")
+        data=request.json
+        #데이터가 답변이라는 변수로 다 들어가서, 유저아이디는 유저아이디/선택아이디는 선택아이디
+        #한 사람의 아이디에는 여러 답변이 있으니께. 요구사항에도 아이디 1개에 답변 1개씩만 출력됨.
+        for answer in data:
+            user_id=answer.get("userId")
+            choice_id=answer.get("choicesId")
+            #존재한지 안한지 확인
+            if not user_id or choice_id:
+                return{"msg":"Not Found user_id or choice_id"}
+            
+            #검사 후 데이터 존재시 답변 정보를 저장한다.
+            new_answer=Answer(user_id=user_id,choice_id=choice_id)
+            db.session.add(new_answer)
+            db.session.commit()
+            return {
+                    "msg":"User: 1's answers Success Create"
+                    },201
